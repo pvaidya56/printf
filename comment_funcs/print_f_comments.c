f3132cb1e079e3adfbb84fe7764fb9ecf4d9ab61
jmgindi <jmgindi@gmail.com>
2018-10-22 16:54:51 +0000

    first outline of printf

---
+++ comment_funcs/print_f_comments.c
@@ -0,0 +1,9 @@
+void printf(const char * const str, ...)
+{
+	//set variables
+	//cycle through char by char until we hit %
+	//scan for valid spacers until we hit flag
+	//add element count from % to flag to @offset
+	//identify flag and return function corresponding to flag
+	//loop
+}
