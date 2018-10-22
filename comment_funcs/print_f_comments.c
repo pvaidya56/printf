54bbb3f15ae1471f8a4599cb390105edf5d88b22
jmgindi <jmgindi@gmail.com>
2018-10-22 17:00:53 +0000

    changed prototype to match

--- comment_funcs/print_f_comments.c
+++ comment_funcs/print_f_comments.c
@@ -1,9 +1,10 @@
-void printf(const char * const str, ...)
+int printf(const char *format, ...)
 {
 	//set variables
 	//cycle through char by char until we hit %
 	//scan for valid spacers until we hit flag
 	//add element count from % to flag to @offset
 	//identify flag and return function corresponding to flag
-	//loop
+	//loop until null
+	//return number of chars printed
 }
