ad9914100b734b1fe41d8ffec6df5f6059eb8833
Priya Vaidya <pvaidya56@gmail.com>
2018-10-23 23:51:04 +0000

    Added comments

--- func_print.c
+++ func_print.c
@@ -2,26 +2,37 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "holberton.h"
+/**
+ * func_print - Matches the conversion specifier with it's function
+ * @k: conversion specifier
+ * @spec: function for each specifier
+ * Return:gcc The function
+ */
 
 int func_print(char k, va_list *spec)
 {
 	int i;
-x
-	spec chars[] = {
+
+	spec chars[] {
 		{"c", buff_i},
 		{"s", buff_s},
+		{"d", buff_d},
+		{"i", buff_i},
 		{'\0', NULL}
 	};
 
-	for(i = 0; chars[i].id != '\0'; i++)
+	if (k == '%')
+        {
+                putchar('%');
+        }
+
+	for (i = 0; chars[i].func != '\0'; i++)
 	{
 		if (chars[i].id == k)
 		{
+
 			return (chars[i].func(spec));
 		}
 	}
-	if (k == '%')
-	{
-		putchar('%');
-	}
+
 }
